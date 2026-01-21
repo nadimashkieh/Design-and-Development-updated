a7bfd597301900c1a2ce253eb690ea95a7a73bbc
nadimashkieh <59630860+nadimashkieh@users.noreply.github.com>
2026-01-21 18:51:22 -0500

    Jan 2026

---
+++ Vaccine_in_Arab_World/Twitter_pilot/.ipynb_checkpoints/config_fas-checkpoint.py
@@ -0,0 +1,12 @@
+
+   
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAE8dbwEAAAAABvNxooVf915gu%2BmtocSXLhnfYK8%3DUzV29whlhV88jczMhtifg0TgynlfC2Z3YTS4e5Vvubfugn9tXk'
+
+
+# Query (Example: '(from:twitterdev -is:retweet) OR #twitterdev')
+query = "لقاحات OR تطعيم OR لقاح OR تطعيمات"
+
+start_time = "2019-12-01T00:00:00Z"
+end_time = "2020-01-01T23:59:59Z"
+
+max_results = 100
---
+++ Vaccine_in_Arab_World/Twitter_pilot/config_fas.py
@@ -0,0 +1,12 @@
+
+   
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAE8dbwEAAAAA5UKzul916MBlqji0beUAwZS0cx0%3DoHyG73xXXCR1CZyoI2ia0E7HwrhOivEUt1Q2umTKryH5WhdSfu'
+
+
+# Query (Example: '(from:twitterdev -is:retweet) OR #twitterdev')
+query = "لقاحات OR تطعيم OR لقاح OR تطعيمات"
+
+start_time = "2022-07-01T00:00:00Z"
+end_time = "2022-10-24T23:59:59Z"
+
+max_results = 100
